aab2bc4c8ba48c9dcc78606eea4386966ef63449
Aida Amini <aamini@f0r1s12.codam.nl>
2025-05-27 20:59:12 +0200

    up to date

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aamini <aamini@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 12:35:33 by aamini            #+#    #+#             */
-/*   Updated: 2025/05/22 17:20:29 by aamini           ###   ########.fr       */
+/*   Updated: 2025/05/27 20:45:22 by aamini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@
 # include "libft/libft.h"
 
 int	ft_printf(const char *str, ...);
-int ft_putchar(char c);
+int	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_putnbr(int n);
 int	ft_putunbr(unsigned int n);
 int	ft_puthex(unsigned int n, int uppercase);
-int ft_puthex_ptr(unsigned long n);
+int	ft_puthex_ptr(unsigned long n);
 int	ft_putptr(void *ptr);
 
 #endif
